e168e6d801d92e21eeb4685ef4ead57dae669de4
DaCoda_Blak <108761924+DacodaBx@users.noreply.github.com>
2023-05-01 18:20:46 -0700

    Add files via upload

---
+++ quotes.js
@@ -0,0 +1,303 @@
+const localQuotes = [
+    {
+        "text": "Defendant Trump Charged With RICO",
+        "author": "The Trump Indictment Game",
+        "author2": "The Game, That's Not a Game",
+    },
+    {
+        "text": "Defendant Trump Supporters Abandon Him",
+        "author": "The Trump Indictment Game",
+        "author2": "The Game, That's Not a Game",
+    },
+
+    {
+        "text": "Defendant Trump Indicted on Stochastic Terrorism Charges",
+        "author": "The Trump Indictment Game",
+        "author2": "The Game, That's Not a Game",
+    },
+    
+    {
+        "text": "Defendant Trump Indicted On Obstruction Charges",
+        "author": "The Trump Indictment Game",
+        "author2": "The Game, That's Not a Game",
+    },
+    {
+        "text": "Defendant Trump Indicted On Racketeering Charges",
+        "author": "The Trump Indictment Game",
+        "author2": "The Game, That's Not a Game",
+    },
+    {
+        "text": "Defendant Trump Slapped With A Gag Order",
+        "author": "The Trump Indictment Game",
+        "author2": "The Game, That's Not a Game",
+    },
+    {
+        "text": "Defendant Trump Under House Arrest",
+        "author": "The Trump Indictment Game The Game, That's Not a Game",
+    },
+    {
+        "text": "Defendant Trump Danger to Society, Held Without Bond",
+        "author": "The Trump Indictment Game",
+        "author2": "The Game, That's Not a Game",
+    },
+    {
+        "text": "Mitt Romney 2024",
+        "author": "The Trump Indictment Game",
+        "author2": "The Game, That's Not a Game",
+    },
+    {
+        "text": "Defendant Trump Produces His Own Mugshot",
+        "author": "The Trump Indictment Game",
+        "author2": "The Game, That's Not a Game",
+    },
+    {
+        "text": "Defendant Trump Held In Contempt of Court",
+        "author": "The Trump Indictment Game",
+        "author2": "The Game, That's Not a Game",
+    },
+    {
+        "text": "Lindsey Graham is Indicted",
+        "author": "The Trump Indictment Game",
+        "author2": "The Game, That's Not a Game",
+    },
+    {
+        "text": "Lindsey Graham Cries, Again on National T.V",
+        "author": "The Trump Indictment Game",
+        "author2": "The Game, That's Not a Game",
+    },
+    {
+        "text": "Jim Jordan is Indicted",
+        "author": "The Trump Indictment Game",
+        "author2": "The Game, That's Not a Game",
+    },
+    {
+        "text": "Rudy Giuliani is Indicted",
+        "author": "The Trump Indictment Game",
+        "author2": "The Game, That's Not a Game",
+    },
+    {
+        "text": "Joe Tacopina Resigns as Trump Lawyer",
+        "author": "The Trump Indictment Game",
+        "author2": "The Game, That's Not a Game",
+    },
+    {
+        "text": "Allen Weisselberg Flips",
+        "author": "The Trump Indictment Game",
+        "author2": "The Game, That's Not a Game",
+    },
+    {
+        "text": "Video Cameras are Allowed in Court",
+        "author": "The Trump Indictment Game",
+        "author2": "The Game, That's Not a Game",
+    },
+    {
+        "text": "Melania Divorces Defendant Trump",
+        "author": "The Trump Indictment Game",
+        "author2": "The Game, That's Not a Game",
+    },
+    {
+        "text": "Rudy Giuliani is a Mole",
+        "author": "The Trump Indictment Game",
+        "author2": "The Game, That's Not a Game",
+    },
+    {
+        "text": "Ted Cruz Tries to Pretend He Never Liked Trump",
+        "author": "The Trump Indictment Game",
+        "author2": "The Game, That's Not a Game",
+    },
+    {
+        "text": "Defendant Trump Loses Secret Service Protection",
+        "author": "The Trump Indictment Game",
+        "author2": "The Game, That's Not a Game",
+    },
+    {
+        "text": "Defendant Trump Flees The Country",
+        "author": "The Trump Indictment Game",
+        "author2": "The Game, That's Not a Game",
+    },
+    {
+        "text": "Defendant Trump Refuses to Leave Mar-A-Lago",
+        "author": "The Trump Indictment Game",
+        "author2": "The Game, That's Not a Game",
+    },
+    {
+        "text": "Defendant Trump Apologizes for Calling for The Execution of Teenagers",
+        "author": "The Trump Indictment Game",
+        "author2": "The Game, That's Not a Game",
+    },
+    {
+        "text": "Defendant Trump Admits that He is a Racist",
+        "author": "The Trump Indictment Game",
+        "author2": "The Game, That's Not a Game",
+    },
+    {
+        "text": "Defendant Trump Inadvertently Admits to More Crimes",
+        "author": "The Trump Indictment Game",
+        "author2": "The Game, That's Not a Game",
+    },
+    {
+        "text": "Defendant Trump Charged With Giving Perjured Testimony in Court",
+        "author": "The Trump Indictment Game",
+        "author2": "The Game, That's Not a Game",
+    },
+    {
+        "text": "GOP Loses the House Majority Due to Overwhelming Indictments",
+        "author": "The Trump Indictment Game",
+        "author2": "The Game, That's Not a Game",
+    },
+    {
+        "text": "Defendant Trump Attempts to Run The Courtroom",
+        "author": "The Trump Indictment Game",
+        "author2": "The Game, That's Not a Game",
+    },
+    {
+        "text": "Defendant Trump Merches the SHIT Out of His Cases",
+        "author": "The Trump Indictment Game",
+        "author2": "The Game, That's Not a Game",
+    },
+    {
+        "text": "Marjorie Taylor Greene Gets Arrested in NYC",
+        "author": "The Trump Indictment Game",
+        "author2": "The Game, That's Not a Game",
+    },
+    {
+        "text": "Gangs From All Over The Country Unite in Peaceful Protest for Equal Justice",
+        "author": "The Trump Indictment Game",
+        "author2": "The Game, That's Not a Game",
+    },
+    {
+        "text": "Fulton County Indictment is Announced Within First Two Weeks of April",
+        "author": "The Trump Indictment Game",
+        "author2": "The Game, That's Not a Game",
+    },
+    {
+        "text": "Defendant Trump is Shackled and Spit Masked",
+        "author": "The Trump Indictment Game",
+        "author2": "The Game, That's Not a Game",
+    },
+    {
+        "text": "Judge Denies Change of Venue",
+        "author": "The Trump Indictment Game",
+        "author2": "The Game, That's Not a Game",
+    },
+    {
+        "text": "Defendant Trump Attorneys Realize that Trump is Incapable of Telling the Truth ",
+        "author": "The Trump Indictment Game",
+        "author2": "The Game, That's Not a Game",
+    },
+    {
+        "text": "Defendant Trump Indicted On Accessory to Murder",
+        "author": "The Trump Indictment Game The Game, That's Not a Game"
+    },
+    {
+        "text": "Ivana Trump is Exhumed",
+        "author": "The Trump Indictment Game",
+        "author2": "The Game, That's Not a Game",
+    },
+    {
+        "text": "Trial Set For The Summer of 23",
+        "author": "The Trump Indictment Game",
+        "author2": "The Game, That's Not a Game",
+    },
+    {
+        "text": "Trial Set For Fall 23",
+        "author": "The Trump Indictment Game",
+        "author2": "The Game, That's Not a Game",
+    },
+    {
+        "text": "Trial Set For Winter 23",
+        "author": "The Trump Indictment Game",
+        "author2": "The Game, That's Not a Game",
+    },
+    {
+        "text": "Defendant Trump Threatens Jury",
+        "author": "The Trump Indictment Game",
+        "author2": "The Game, That's Not a Game",
+    },
+    {
+        "text": "Death Threat to DA, Found to be Linked Directly to Defendant Trump",
+        "author": "The Trump Indictment Game",
+        "author2": "The Game, That's Not a Game",
+    },
+    {
+        "text": "Defendant Trump Found To Be, Guilty AF",
+        "author": "The Trump Indictment Game",
+        "author2": "The Game, That's Not a Game",
+    },
+    {
+        "text": "Defendant Trump Indicted On Penal Code 195.05",
+        "author": "The Trump Indictment Game",
+        "author2": "The Game, That's Not a Game",
+    },
+    {
+        "text": "Defendant Trump's Attorneys Jump Ship",
+        "author": "The Trump Indictment Game",
+        "author2": "The Game, That's Not a Game",
+    },
+    {
+        "text": "Defendant Trump's Attorney Charged With Perjury",
+        "author": "The Trump Indictment Game",
+        "author2": "The Game, That's Not a Game",
+    },
+    {
+        "text": "Defendant Trump Indicted On Sedition Charges",
+        "author": "The Trump Indictment Game",
+        "author2": "The Game, That's Not a Game",
+    },
+    {
+        "text": "Trump Father's Love Child With a Black Woman",
+        "author": "The Trump Indictment Game",
+        "author2": "The Game, That's Not a Game",
+    },
+    {
+        "text": "Jeff Sessions Becomes a Witness Against Defendant Trump",
+        "author": "The Trump Indictment Game",
+        "author2": "The Game, That's Not a Game",
+    },
+    {
+        "text": "Bill Barr Becomes a Witness Against Defendant Trump",
+        "author": "The Trump Indictment Game",
+        "author2": "The Game, That's Not a Game",
+    },
+    {
+        "text": "Defendant Trump Indicted On NY Penal Code 175.10",
+        "author": "The Trump Indictment Game",
+        "author2": "The Game, That's Not a Game",
+    },
+    {
+        "text": "Defendant Trump Indicted On NY Penal Code 175.3",
+        "author": "The Trump Indictment Game",
+        "author2": "The Game, That's Not a Game",
+    },
+    {
+        "text": "Defendant Trump Indicted On NY Penal Code 175.45",
+        "author": "The Trump Indictment Game",
+        "author2": "The Game, That's Not a Game",
+    },
+    {
+        "text": "Defendant Trump Pleads Insanity",
+        "author": "The Trump Indictment Game",
+        "author2": "The Game, That's Not a Game",
+    },
+    {
+        "text": "Judge Merchan Issues A Bench Warrant for Defendant Trump",
+        "author": "The Trump Indictment Game",
+        "author2": "The Game, That's Not a Game",
+    },
+    {
+        "text": "NRA Parts Ways With Defendant Trump",
+        "author": "The Trump Indictment Game",
+        "author2": "The Game, That's Not a Game",
+    },
+    {
+        "text": "Defendant Trump Arrested At His Rally",
+        "author": "The Trump Indictment Game",
+        "author2": "The Game, That's Not a Game",
+    },
+    {
+        "text": "Democrats & Republicans Unite Against Defendant Trump",
+        "author": "The Trump Indictment Game",
+        "author2": "The Game, That's Not a Game",
+    },
+    
+]
